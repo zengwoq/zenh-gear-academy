72e58debc9bf929b56d8122dc051379942d290ce
zengwoq <wonderfulp7@outlook.com>
2024-08-09 10:01:50 +0800

    Update lib.rs

--- wordle-game-program/game-session/src/lib.rs
+++ wordle-game-program/game-session/src/lib.rs
@@ -1,20 +1,19 @@
 #![no_std]
-use gstd::{exec, msg, prelude::*, debug, ActorId, collections::HashMap};
-use wordle_io::{Action, Event};
 use game_session_io::*;
+use gstd::{collections::HashMap, debug, exec, msg, prelude::*, ActorId};
+use wordle_io::{Action, Event};
 
 static mut GAME_SESSION_STATE: Option<GameSessionState> = None;
 
 // 最大检查次数
 const MAX_CHECK_COUNT: u8 = 6;
 
-
 #[no_mangle]
 extern "C" fn init() {
     let wordle_program = msg::load().expect("Unable to decode init");
     unsafe {
         GAME_SESSION_STATE = Some(GameSessionState {
-            wordle_program: wordle_program,
+            wordle_program,
             user_to_session: HashMap::new(),
         });
     }
@@ -22,30 +21,38 @@ extern "C" fn init() {
 }
 
 fn handle_start_game() {
-    let state = unsafe {GAME_SESSION_STATE.as_mut().expect("GAME_SESSION_STATE is not initialized")};
+    let state = unsafe {
+        GAME_SESSION_STATE
+            .as_mut()
+            .expect("GAME_SESSION_STATE is not initialized")
+    };
     let user = msg::source();
-    
+
     if !state.user_to_session.contains_key(&user) {
-        state.user_to_session.insert(user, Session {
-            start_block: 0,
-            check_count: 0,
-            msg_ids: (0.into(), 0.into()),
-            status: SessionStatus::StartGameWaiting,
-            result: SessionResult::Ongoing,
-        });
+        state.user_to_session.insert(
+            user,
+            Session {
+                start_block: 0,
+                check_count: 0,
+                msg_ids: (0.into(), 0.into()),
+                status: SessionStatus::StartGameWaiting,
+                result: SessionResult::Ongoing,
+            },
+        );
     }
 
     let session: &mut Session = state.user_to_session.get_mut(&user).unwrap();
-    
+
     match &session.status {
         SessionStatus::StartGameWaiting | SessionStatus::CheckWordWaiting => {
             // 向 Wordle 程序发送开始游戏的请求
-            let msg_id: gstd::MessageId = msg::send(state.wordle_program, Action::StartGame { user }, 0)
-                .expect("handle_start_game: error in sending `Action::StartGame`");
+            let msg_id: gstd::MessageId =
+                msg::send(state.wordle_program, Action::StartGame { user }, 0)
+                    .expect("handle_start_game: error in sending `Action::StartGame`");
             session.msg_ids = (msg_id, msg::id());
             session.status = SessionStatus::StartGameSent;
             exec::wait();
-        },
+        }
         // 如果已收到回复，处理游戏开始事件
         SessionStatus::ReplyReceived(recv_event) => {
             if let SessionEvent::GameStarted = recv_event {
@@ -54,21 +61,33 @@ fn handle_start_game() {
                 session.msg_ids = (0.into(), 0.into());
                 session.status = SessionStatus::CheckWordWaiting;
                 session.result = SessionResult::Ongoing;
-                msg::reply(SessionEvent::GameStarted , 0).expect("Error in sending `GameStarted` reply");
-                
-                msg::send_delayed(exec::program_id(), SessionAction::CheckGameStatus { user }, 0, CHECK_GAME_STATUS_DELAY)
-                    .expect("handle_start_game: error in sending `SessionAction::CheckGameStatus`");
-                
+                msg::reply(SessionEvent::GameStarted, 0)
+                    .expect("Error in sending `GameStarted` reply");
+
+                msg::send_delayed(
+                    exec::program_id(),
+                    SessionAction::CheckGameStatus { user },
+                    0,
+                    CHECK_GAME_STATUS_DELAY,
+                )
+                .expect("handle_start_game: error in sending `SessionAction::CheckGameStatus`");
             } else {
-                panic!("handle_start_game: invalid received event: {:x?}", recv_event);
+                panic!(
+                    "handle_start_game: invalid received event: {:x?}",
+                    recv_event
+                );
             }
-        },
+        }
         _ => panic!("handle_start_game: wrong status, {:x?}", session.status),
     }
 }
 
 fn handle_check_word(word: String) {
-    let state = unsafe {GAME_SESSION_STATE.as_mut().expect("GAME_SESSION_STATE is not initialized")};
+    let state = unsafe {
+        GAME_SESSION_STATE
+            .as_mut()
+            .expect("GAME_SESSION_STATE is not initialized")
+    };
     let user = msg::source();
 
     if !state.user_to_session.contains_key(&user) {
@@ -76,7 +95,7 @@ fn handle_check_word(word: String) {
     }
 
     let session: &mut Session = state.user_to_session.get_mut(&user).unwrap();
-    
+
     match &session.status {
         SessionStatus::CheckWordWaiting => {
             if word.len() != 5 || !word.chars().all(|c| c.is_lowercase()) {
@@ -85,11 +104,18 @@ fn handle_check_word(word: String) {
 
             session.check_count += 1;
             // 如果超过最大检查次数或时间超时，游戏失败
-            if session.check_count > MAX_CHECK_COUNT || exec::block_height() > session.start_block + CHECK_GAME_STATUS_DELAY {
+            if session.check_count > MAX_CHECK_COUNT
+                || exec::block_height() > session.start_block + CHECK_GAME_STATUS_DELAY
+            {
                 session.status = SessionStatus::StartGameWaiting;
                 session.result = SessionResult::Lose;
-                msg::reply(SessionEvent::GameOver { result: SessionResult::Lose }, 0)
-                    .expect("handle_check_word: error in replying `SessionEvent::GameOver`");
+                msg::reply(
+                    SessionEvent::GameOver {
+                        result: SessionResult::Lose,
+                    },
+                    0,
+                )
+                .expect("handle_check_word: error in replying `SessionEvent::GameOver`");
             } else {
                 let msg_id = msg::send(state.wordle_program, Action::CheckWord { user, word }, 0)
                     .expect("handle_check_word: error in sending `Action::CheckWord`");
@@ -99,22 +125,35 @@ fn handle_check_word(word: String) {
                 debug!("handle_check_word: `CheckWord` wait");
                 exec::wait();
             }
-        },
+        }
         // 处理检查单词后的回复
         SessionStatus::ReplyReceived(recv_event) => {
-            if let SessionEvent::WordChecked { correct_positions, contained_in_word } = recv_event {
-
+            if let SessionEvent::WordChecked {
+                correct_positions,
+                contained_in_word,
+            } = recv_event
+            {
                 session.msg_ids = (0.into(), 0.into());
                 if correct_positions.len() == 5 {
                     session.status = SessionStatus::StartGameWaiting;
                     session.result = SessionResult::Win;
-                    msg::reply(SessionEvent::GameOver { result: SessionResult::Lose, }, 0)
-                        .expect("handle_check_word: error in replying `GameOver(Win)`");
+                    msg::reply(
+                        SessionEvent::GameOver {
+                            result: SessionResult::Lose,
+                        },
+                        0,
+                    )
+                    .expect("handle_check_word: error in replying `GameOver(Win)`");
                 } else if session.check_count >= 6 {
                     session.status = SessionStatus::StartGameWaiting;
                     session.result = SessionResult::Lose;
-                    msg::reply(SessionEvent::GameOver { result: SessionResult::Lose }, 0)
-                        .expect("handle_check_word: error in replying `GameOver(Lose)`");
+                    msg::reply(
+                        SessionEvent::GameOver {
+                            result: SessionResult::Lose,
+                        },
+                        0,
+                    )
+                    .expect("handle_check_word: error in replying `GameOver(Lose)`");
                 } else {
                     let event = SessionEvent::WordChecked {
                         correct_positions: correct_positions.to_vec(),
@@ -122,12 +161,16 @@ fn handle_check_word(word: String) {
                     };
                     session.status = SessionStatus::CheckWordWaiting;
                     session.result = SessionResult::Ongoing;
-                    msg::reply(event, 0).expect("handle_check_word: error in replying `WordChecked`");
+                    msg::reply(event, 0)
+                        .expect("handle_check_word: error in replying `WordChecked`");
                 }
             } else {
-                panic!("handle_check_word: invalid ReplyReceived event: {:x?}", recv_event);
+                panic!(
+                    "handle_check_word: invalid ReplyReceived event: {:x?}",
+                    recv_event
+                );
             }
-        },
+        }
         // 如果状态不正确，抛出错误
         _ => panic!("handle_check_word: wrong status ({:x?})", session.status),
     }
@@ -136,19 +179,34 @@ fn handle_check_word(word: String) {
 // 检查游戏状态
 fn handle_check_game_status(user: &ActorId) {
     debug!("handle_check_game_status");
-    let state = unsafe {GAME_SESSION_STATE.as_mut()
-        .expect("handle_check_game_status: GAME_SESSION_STATE is not initialized")};
+    let state = unsafe {
+        GAME_SESSION_STATE
+            .as_mut()
+            .expect("handle_check_game_status: GAME_SESSION_STATE is not initialized")
+    };
     if !state.user_to_session.contains_key(user) {
         panic!("handle_check_game_status: non-existing user");
     }
 
     let session: &mut Session = state.user_to_session.get_mut(user).unwrap();
-    debug!("handle_check_game_status: block_height={}, start_block={}", exec::block_height(), session.start_block);
-    if exec::block_height() > session.start_block + CHECK_GAME_STATUS_DELAY && session.result == SessionResult::Ongoing {
+    debug!(
+        "handle_check_game_status: block_height={}, start_block={}",
+        exec::block_height(),
+        session.start_block
+    );
+    if exec::block_height() > session.start_block + CHECK_GAME_STATUS_DELAY
+        && session.result == SessionResult::Ongoing
+    {
         session.result = SessionResult::Lose;
         session.status = SessionStatus::StartGameWaiting;
-        msg::send(*user, SessionEvent::GameOver { result: SessionResult::Lose, }, 0)
-            .expect("handle_check_game_status: error in sending `GameOver(Lose)`");
+        msg::send(
+            *user,
+            SessionEvent::GameOver {
+                result: SessionResult::Lose,
+            },
+            0,
+        )
+        .expect("handle_check_game_status: error in sending `GameOver(Lose)`");
     }
 }
 
@@ -163,14 +221,18 @@ extern "C" fn handle() {
         // 检查单词
         SessionAction::CheckWord { word } => handle_check_word(word.to_string()),
         // 检查游戏状态
-        SessionAction::CheckGameStatus { user } => handle_check_game_status(&user),
+        SessionAction::CheckGameStatus { user } => handle_check_game_status(user),
     }
 }
 
 #[no_mangle]
 extern "C" fn handle_reply() {
     debug!("handle_reply");
-    let state = unsafe {GAME_SESSION_STATE.as_mut().expect("GAME_SESSION_STATE is not initialized")};
+    let state = unsafe {
+        GAME_SESSION_STATE
+            .as_mut()
+            .expect("GAME_SESSION_STATE is not initialized")
+    };
     let reply_to = msg::reply_to().expect("Failed to query reply_to data");
     let reply_message: Event = msg::load().expect("Unable to decode wordle's reply message");
     debug!("handle_reply: reply_message {:x?}", reply_message);
@@ -188,9 +250,13 @@ extern "C" fn handle_reply() {
             } else {
                 panic!("handle_reply: GameStarted, non existing user");
             }
-        },
+        }
         // 检查单词回复
-        Event::WordChecked { user, correct_positions, contained_in_word } => {
+        Event::WordChecked {
+            user,
+            correct_positions,
+            contained_in_word,
+        } => {
             if let Some(session) = state.user_to_session.get_mut(user) {
                 if reply_to == session.msg_ids.0 {
                     let event = SessionEvent::WordChecked {
@@ -205,17 +271,20 @@ extern "C" fn handle_reply() {
             } else {
                 panic!("handle_reply: WordChecked, non existing user");
             }
-        },
+        }
     }
 }
 
 #[no_mangle]
 extern "C" fn state() {
-    let game_session = unsafe { GAME_SESSION_STATE.take().expect("Unexpected error in taking state") };
+    let game_session = unsafe {
+        GAME_SESSION_STATE
+            .take()
+            .expect("Unexpected error in taking state")
+    };
     msg::reply::<State>(game_session.into(), 0)
         .expect("Failed to encode or reply with `GameSessionState` from `state()`");
 }
-
 
 #[cfg(test)]
 const CHECK_GAME_STATUS_DELAY: u32 = 2;
